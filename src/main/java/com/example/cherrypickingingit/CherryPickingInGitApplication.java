85ae8fc95c1faa9560eef11ed50c0c3caa9c86cd
Ravi Kumar Gupta <babaravibbb@gmail.com>
2023-08-16 23:34:08 +0530

    first commit

---
+++ src/main/java/com/example/cherrypickingingit/CherryPickingInGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.cherrypickingingit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryPickingInGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherryPickingInGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cherrypickingingit/CherryPickingInGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cherrypickingingit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryPickingInGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
