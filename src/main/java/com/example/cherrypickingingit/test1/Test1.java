69ca7a476b61c2e6fb2cbd6b9006c2670cfa7d41
Ravi Kumar Gupta <babaravibbb@gmail.com>
2024-07-31 14:54:18 +0530

    test1c3

--- src/main/java/com/example/cherrypickingingit/test1/Test1.java
+++ src/main/java/com/example/cherrypickingingit/test1/Test1.java
@@ -5,4 +5,15 @@ public class Test1 {
     public static void display(){
         System.out.println("test1c1");
     }
+
+
+    public static void display2(){
+        System.out.println("test1c2");
+    }
+
+    public static void display3(){
+        System.out.println("test1c3");
+    }
+
+
 }
